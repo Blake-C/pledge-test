7c42948e27a800c51e25fd47bb1f93ecf9457399
Blake Cerecero <Blake@DigitalBlake.com>
2020-02-08 18:50:29 -0600

    [js, css] Added pledge bars + animation
    I was trying to do this with the percentage, but the bars appears much smaller than what the design called for.
    
    This need to be refactored for cases where the pledge amounts are so large the pixel values would cause the bars to go off the screen.
    
    I would not call this production ready

--- index.js
+++ index.js
@@ -13,3 +13,16 @@
 		}
 	}, 4)
 
+	// Pledge Bars
+	var bar_graph = document.querySelectorAll('.js-leaderboard__bar-graph')
+	var bar_graph_array = Array.prototype.slice.call(bar_graph)
+
+	bar_graph_array.map(function(element) {
+		var pledge_amount = element.dataset.pledge
+		var pledge_amount_int = parseInt(pledge_amount)
+
+		setTimeout(function() {
+			element.style.width = pledge_amount_int + 'px'
+		}, 100)
+	})
+})()
