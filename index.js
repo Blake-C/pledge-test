4af4705997e52869cbf17cbec1206e6c11f21e14
Blake Cerecero <Blake@DigitalBlake.com>
2020-02-09 18:08:44 -0600

    [js] Updated & simplified page scripts
    - Removed pledge_total_value_int and called parseInt on pledge_total_value
    - Updated start_value to pledge_start_value
    - Removed pledge_amount_int and called parseInt on pledge_amount
    - Created select_list_array instead of overriding the select_list variable
    - Removed miscellaneous console.log

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 ;(function() {
 	// Stats Counter
 	var stats = document.getElementById('js-pledges-stats')
-	var pledge_total_value = stats.dataset.current
-	var pledge_total_value_int = parseInt(pledge_total_value)
-	var start_value = 0
+	var pledge_total_value = parseInt(stats.dataset.current)
+	var pledge_start_value = 0
 
 	var stats_interval = setInterval(function() {
-		if (start_value !== pledge_total_value_int) {
-			start_value++
-			stats.innerText = start_value.toLocaleString('en-IN')
+		if (pledge_start_value !== pledge_total_value) {
+			pledge_start_value++
+			stats.innerText = pledge_start_value.toLocaleString('en-IN')
 		} else {
 			clearInterval(stats_interval)
 		}
@@ -19,11 +18,10 @@
 	var bar_graph_array = Array.prototype.slice.call(bar_graph)
 
 	bar_graph_array.map(function(element) {
-		var pledge_amount = element.dataset.pledge
-		var pledge_amount_int = parseInt(pledge_amount)
+		var pledge_amount = parseInt(element.dataset.pledge)
 
 		setTimeout(function() {
-			element.style.width = pledge_amount_int + 'px'
+			element.style.width = pledge_amount + 'px'
 		}, 100)
 	})
 
@@ -42,15 +40,14 @@
 
 	// Select list initial color
 	var select_list = document.querySelectorAll('.js-district-select')
-	var select_list = Array.prototype.slice.call(select_list)
+	var select_list_array = Array.prototype.slice.call(select_list)
 
-	select_list.map(function(element) {
+	select_list_array.map(function(element) {
 		if (element.classList.contains('clicked')) {
 			return
 		}
 
 		element.addEventListener('focus', function() {
-			console.log('here')
 			element.classList.add('clicked')
 		})
 	})
